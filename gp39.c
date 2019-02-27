9a467078d8dd294f2957746c0b765617e23f10ee
arunkumargovindharaj <46838557+arunkumargovindharaj@users.noreply.github.com>
2019-02-27 16:33:15 +0600

    Create gp39.c

---
+++ gp39.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int num,i,j,k=0,f=0;
+	scanf("%d",&num);
+for(i=0;i<=num;i++)
+{
+    for(i=0;i<=num;i++)
+    {
+	    if(i*i==num)
+         {
+             f=1;
+         }
+}}
+if(f==1)
+{
+    printf("yes");
+}
+
+else
+{
+    printf("no");
+}
+    return 0;
+}
+	
