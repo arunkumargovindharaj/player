be25ae4ea4a5622583561c7089371baf635e9cba
arunkumargovindharaj <46838557+arunkumargovindharaj@users.noreply.github.com>
2019-03-16 16:33:22 +0530

    Create gp100.c

---
+++ gp100.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   long int n,rem=0,i=1,sum=0;
+
+	scanf("%ld",&n);
+	while(n!=0)
+	{
+		rem=n%10;
+		sum=sum+rem*i;
+		n=n/10;
+		i=i*2;
+	}
+	printf("%lx",sum);
+
+
+    return 0;
+}
