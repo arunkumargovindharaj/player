3d63fd152546bae11bff4bf2bca42946ee0cb6c7
arunkumargovindharaj <46838557+arunkumargovindharaj@users.noreply.github.com>
2019-03-22 08:34:56 +0530

    Update GP103.C

--- GP103.C
+++ GP103.C
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int i,j,n,a[100],sum=0;
+int i,j,n,b[100],sum=0;
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
-scanf("%d",&a[i]);
+scanf("%d",&b[i]);
 }
 for(i=0;i<=n;i++)
 {
-sum=sum+a[i+1];
+sum=sum+b[i+1];
 }
 printf("%d",sum);
 return 0;
 }
+
